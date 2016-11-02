36138742d559a36bc83496cf1988461115090a00
Armi91 <daw.armatys@gmail.com>
2016-11-02 15:37:51 +0100

    First Commit

---
+++ src/js/scripts.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($) {
+
+// MOBILE MENU
+
+	$('.hamburger').click(function(event) {
+		$('nav').toggleClass('active-menu');
+		var h = $('nav').height();
+		var $target = $('.hamburger');
+		var $body = $('body');
+
+		if ($target.css('margin-top') === '0px') {
+			$target.css('margin-top', h + 'px');
+			$body.css('margin-top', h + 'px');
+		} else {
+			$target.css('margin-top', '0px');
+			$body.css('margin-top', '0px');
+		}
+	});
+
+// SLIDER
+
+var $sliderItem = $('#slider > li'),
+	$paginationItem = $('#slider-pagination > li');
+
+var $prev;
+
+$paginationItem.each(function(index, el) {
+	$(this).click(function(event) {
+		event.preventDefault();
+		$paginationItem.removeClass('pagination-active');
+		$(this).addClass('pagination-active');
+		if (!$prev) {
+			$prev = $('#slider > li.slider-active');
+		}
+		$prev.removeClass('slider-active');
+		$sliderItem.eq(index).addClass('slider-active animation-start slide-in')
+			.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+				$(this).removeClass('animation-start slide-in');
+				$prev = $(this);
+			});
+	});
+});
+
+// ScrollTo
+
+var $menu = $('#main-menu > li > a');
+
+$menu.click(function(event) {
+	event.preventDefault();
+	var $link = $(this).attr('href');
+	console.log($link);
+	$.scrollTo($link, {
+		duration: 1000
+	});
+});
+
+
+
+
+});
